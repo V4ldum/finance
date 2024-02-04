409783b91850292cf990135cdc40ba725d6cdbeb
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-02-04 17:51:32 +0100

    fix: removed api key

--- lib/shared/constant/app_string.dart
+++ lib/shared/constant/app_string.dart
@@ -3,7 +3,6 @@ class AppString {
 
   static const numistaApiUrl = 'https://api.numista.com/api/v3';
   static const numistaKeyHeader = 'Numista-API-Key';
-  static const numistaSecretKey = '8opdQA8sX0RmGSPC89Z0EuNkW98rxEWlbD5hqZni'; // TODO(val): remove
 
   static const finaryApiUrl = 'https://api.finary.com';
   static const finaryAppUrl = 'https://app.finary.com';
