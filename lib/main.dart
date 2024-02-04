e54ef07020ab07533d30561da19bf11eb44e1194
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-02-04 16:40:26 +0100

    feature(privacy_shield): Added privacy shield page back, but only for Android this time

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:finance/_l10n/_generated/l10n.dart';
+import 'package:finance/feature/dashboard/presentation/page/privacy_shield_page.dart';
 import 'package:finance/shared/utils/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -18,6 +21,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      builder: (context, child) {
+        if (!Platform.isAndroid) {
+          return child!;
+        }
+        return PrivacyShieldManager(child: child!);
+      },
       localizationsDelegates: const [
         S.delegate,
         GlobalCupertinoLocalizations.delegate,
@@ -32,6 +41,60 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routerConfig: router,
+    );
+  }
+}
+
+class PrivacyShieldManager extends ConsumerStatefulWidget {
+  const PrivacyShieldManager({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  ConsumerState<PrivacyShieldManager> createState() => _PrivacyShieldManagerState();
+}
+
+class _PrivacyShieldManagerState extends ConsumerState<PrivacyShieldManager> {
+  late final AppLifecycleListener _listener;
+  bool shieldUp = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _listener = AppLifecycleListener(
+      onResume: () {
+        setState(() {
+          shieldUp = false;
+        });
+      },
+      onInactive: () {
+        setState(() {
+          shieldUp = true;
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _listener.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        widget.child,
+        AnimatedOpacity(
+          opacity: shieldUp ? 1 : 0,
+          duration: shieldUp ? const Duration(milliseconds: 50) : const Duration(milliseconds: 150),
+          child: const PrivacyShieldPage(),
+        ),
+      ],
     );
   }
 }
--- lib/shared/utils/go_router.dart
+++ lib/shared/utils/go_router.dart
@@ -1,4 +1,5 @@
 import 'package:finance/feature/dashboard/presentation/page/dashboard_page.dart';
+import 'package:finance/feature/dashboard/presentation/page/privacy_shield_page.dart';
 import 'package:finance/feature/settings/presentation/page/alerts_settings_page.dart';
 import 'package:finance/feature/settings/presentation/page/authentication_settings_page.dart';
 import 'package:finance/feature/settings/presentation/page/dashboard_settings_page.dart';
@@ -13,6 +14,14 @@ import 'package:go_router/go_router.dart';
 final router = GoRouter(
   initialLocation: AppRoute._loadingPath,
   routes: [
+    GoRoute(
+      name: AppRoute.privacyShield,
+      path: AppRoute._privacyShield,
+      pageBuilder: (context, state) => CustomTransitionPage(
+        transitionsBuilder: (_, opacity, __, child) => FadeTransition(opacity: opacity, child: child),
+        child: const PrivacyShieldPage(),
+      ),
+    ),
     GoRoute(
       name: AppRoute.loading,
       path: AppRoute._loadingPath,
@@ -67,6 +76,9 @@ class AppRoute {
 
   static const String loading = 'loading';
   static const String _loadingPath = '/';
+
+  static const String privacyShield = 'privacy_shield';
+  static const String _privacyShield = '/hidden';
 
   static const String dashboard = 'dashboard';
   static const String _dashboardPath = 'dashboard';
